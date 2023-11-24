6aebf99abefdb7cabf470a43abad5c7574810c7f
francesco-ismb <francesco.lubrano@linksfoundation.com>
2023-11-24 11:22:50 +0000

    small changes

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default (props) => (
-  <div className="input">
-    <hr />
-    Hi I'm an Input component with the headline:
-    <p>{props.block.headline}</p>
-    <input />
-  </div>
-);
+
+export default function Form(props) {
+  const [firstInput, setFirstInput] = useState('');
+
+  return (
+    <div className="input">
+      <hr />
+      Hi I'm an Input component with the headline:
+      <p>{props.block.headline} <input 
+        value={firstInput}
+        onChange={e => setFirstInput(e.target.value)}
+      /></p>
+      {firstInput}
+    </div>
+  )
+}
